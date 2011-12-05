f7962d39e56827e29c459e5164ff39afc288b363
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2011-12-05 13:26:46 +0900

    DelayProcess
    under construction

---
+++ customProcess/DelayProcess/DelayProcess.cpp
@@ -0,0 +1,99 @@
+#include <boost/shared_ptr.hpp>
+
+#include "libecs.hpp"
+#include "Process.hpp"
+
+#include "Model.hpp"
+
+USE_LIBECS;
+
+LIBECS_DM_CLASS( DelayProcess, Process )
+{
+
+ public:
+
+	LIBECS_DM_OBJECT( DelayProcess, Process )
+	{
+		INHERIT_PROPERTIES( Process ); 
+
+		PROPERTYSLOT_SET_GET( Real, tau );
+	}
+	
+	DelayProcess()
+		:
+		tau( 0.0 )
+	{
+		// do nothing
+	}
+
+	SIMPLE_SET_GET_METHOD( Real, tau );
+	
+	virtual void initialize()
+	{
+		Process::initialize();
+
+		getModel()->getLoggerBroker().createLogger( 
+			FullPN( "Variable:/:t:Value" ), 
+			Logger::Policy() 
+		);
+		
+		o = getVariableReference( "original" ).getVariable();
+		d = getVariableReference( "delayed" ).getVariable();
+
+		// Real t = getModel()->getCurrentTime();
+
+		/*
+		if ( onset >= t->getValue() ) {
+		
+			_nextOnset = onset;
+		
+		} else {
+
+			_nextOnset = onset + floor( ( t->getValue() - onset ) / interval ) * interval;
+		}
+
+		_nextOffset = _nextOnset + offset - onset;
+		*/
+	}
+
+	virtual void fire()
+	{
+		_t = getModel()->getCurrentTime();
+
+		//Logger* theLogger = getModel()->getLoggerBroker().getLogger( 
+		//	FullPN( "Variable:/:t:Value" ));
+
+		if ( _t <= tau ) {
+
+			_theDataSlice = getModel()->getLoggerBroker()->.getLogger( 
+				FullPN( "Variable:/:t:Value" )).getData();
+
+		} else {
+
+			_theDataSlice = getModel()->getLoggerBroker()->getLogger( 
+				FullPN( "Variable:/:t:Value" )).getData( _t - tau, _t );
+
+		}
+
+		d->setValue( _t );
+	}
+
+ protected:
+
+	Variable* o;
+	Variable* d;
+
+	Real tau;
+
+	//LoggerBroker& theLoggerBroker;
+	Logger* const theLogger;
+
+
+ private:
+	
+	Real _t;
+	boost::shared_ptr< DataPointVector > _theDataSlice;
+};
+
+LIBECS_DM_INIT( DelayProcess, Process );
+
---
+++ customProcess/DelayProcess/DelayProcess_Test.em
@@ -0,0 +1,47 @@
+#
+# A very simple model with one michaelis-uni-uni reaction.
+#
+
+# Stepper ODEStepper( DE1 )
+Stepper FixedODE1Stepper( ODE )
+{
+	# no property
+}
+
+Stepper DiscreteTimeStepper( DT )
+{
+	# no property
+}
+
+System System( / )
+{
+	StepperID	ODE;
+
+	Variable Variable( t )
+	{
+		Value	0.0;
+	}
+	
+	Variable Variable( delay_t )
+	{
+		Value	0.0;
+	}
+	
+	Process ExpressionFluxProcess( t )
+	{
+		VariableReferenceList	[ t :.:t 1 ];
+                Expression "1.0";
+	}
+
+	Process DelayProcess( delay )
+	{
+		StepperID	DT;
+
+		VariableReferenceList
+			[ original :.:t 0 ]
+			[ delayed :.:delay_t 1 ];
+
+		tau  20.0;
+	}
+}
+
